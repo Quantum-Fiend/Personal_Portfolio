b121172351040f1b76edbc2ed24b8d4407784650
Troff_Junkie <tusharbisht706@gmail.com>
2026-04-09 15:56:27 +0530

    FIxed

--- src/Components/ui/SocialBar.tsx
+++ src/Components/ui/SocialBar.tsx
@@ -10,42 +10,42 @@ export default function SocialBar() {
       <div className="fixed right-6 top-1/2 -translate-y-1/2 hidden md:flex flex-col items-center gap-5 z-50">
         <div className="w-px h-16 bg-white/30"></div>
 
-        <link href="#" className="icon email">
+        <a href="#" className="icon email">
           <MdEmail />
-        </link>
-        <link href="#" className="icon linkedin">
+        </a>
+        <a href="#" className="icon linkedin">
           <FaLinkedinIn />
-        </link>
-        <link href="#" className="icon github">
+        </a>
+        <a href="#" className="icon github">
           <FaGithub />
-        </link>
-        <link href="#" className="icon discord">
+        </a>
+        <a href="#" className="icon discord">
           <FaDiscord />
-        </link>
-        <link href="#" className="icon instagram">
+        </a>
+        <a href="#" className="icon instagram">
           <FaInstagram />
-        </link>
+        </a>
 
         <div className="w-px h-16 bg-white/30"></div>
       </div>
 
       {/* Mobile (Bottom Bar) */}
       <div className="fixed bottom-4 left-1/2 -translate-x-1/2 flex md:hidden gap-4 bg-black/60 backdrop-blur-md px-4 py-2 rounded-full border border-white/20 z-50">
-        <link href="#" className="icon email">
+        <a href="#" className="icon email">
           <MdEmail />
-        </link>
-        <link href="#" className="icon linkedin">
+        </a>
+        <a href="#" className="icon linkedin">
           <FaLinkedinIn />
-        </link>
-        <link href="#" className="icon github">
+        </a>
+        <a href="#" className="icon github">
           <FaGithub />
-        </link>
-        <link href="#" className="icon discord">
+        </a>
+        <a href="#" className="icon discord">
           <FaDiscord />
-        </link>
-        <link href="#" className="icon instagram">
+        </a>
+        <a href="#" className="icon instagram">
           <FaInstagram />
-        </link>
+        </a>
       </div>
     </>
   );
