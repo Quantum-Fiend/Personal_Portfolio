61a2364e663a2b6a64d41363195ffd1fcf8a842c
Troff_Junkie <tusharbisht706@gmail.com>
2026-04-14 17:21:14 +0530

    FIx Article section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import AboutSection from "@/components/features/home/about/AboutSection";
+import ArticlesSection from "@/components/features/home/article/ArticlesSection";
 import Hero from "@/components/features/home/Hero";
 import ProjectsSection from "@/components/features/home/project/ProjectsSection";
 import SkillsSection from "@/components/features/home/skills/SkillsSection";
@@ -10,6 +11,7 @@ const page = () => {
       <AboutSection />
       <SkillsSection />
       <ProjectsSection />
+      <ArticlesSection />
     </div>
   );
 };
