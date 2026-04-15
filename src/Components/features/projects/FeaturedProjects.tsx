098b04ad680418ec427839bac1f03a4829e813f9
Troff_Junkie <tusharbisht706@gmail.com>
2026-04-15 12:39:35 +0530

    Clean Up

--- src/Components/features/projects/FeaturedProjects.tsx
+++ src/Components/features/projects/FeaturedProjects.tsx
@@ -1,6 +1,3 @@
-"use client";
-
-import { motion } from "framer-motion";
 import ProjectCard from "./ProjectCard";
 import { featuredProjects } from "./projects.data";
 
--- src/Components/features/projects/GithubProjects.tsx
+++ src/Components/features/projects/GithubProjects.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import ProjectCard from "./ProjectCard";
 import { githubProjects } from "./projects.data";
 
