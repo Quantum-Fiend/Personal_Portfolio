a0ef8d7cdb70eabb0dbb617e9f807ced28e6c5ed
Troff_Junkie <tusharbisht706@gmail.com>
2026-04-20 17:51:41 +0530

    Article Section Off Fully

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import AboutSection from "@/components/features/home/about/AboutSection";
-import ArticlesSection from "@/components/features/home/article/ArticlesSection";
+//import ArticlesSection from "@/components/features/home/article/ArticlesSection";
 import Hero from "@/components/features/home/Hero";
 import ProjectsSection from "@/components/features/home/project/ProjectsSection";
 import SkillsSection from "@/components/features/home/skills/SkillsSection";
@@ -16,7 +16,7 @@ const page = () => {
       <AboutSection />
       <SkillsSection />
       <ProjectsSection />
-      <ArticlesSection />
+      {/*<ArticlesSection />*/}
     </div>
   );
 };
