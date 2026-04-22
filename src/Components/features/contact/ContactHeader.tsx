6162e558a15beea003020fdc0257286cb514e6fb
Troff_Junkie <tusharbisht706@gmail.com>
2026-04-22 15:35:33 +0530

    Fixed Contact Page Text

--- src/Components/features/contact/ContactHeader.tsx
+++ src/Components/features/contact/ContactHeader.tsx
@@ -9,13 +9,13 @@ export default function ContactHeader() {
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
     >
-      <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold tracking-tight">
+      <h1 className="text-4xl sm:text-6xl md:text-7xl font-bold tracking-tight">
         Let’s Build <span className="text-gray-400">Something Great</span>
       </h1>
 
       <p className="text-gray-400 mt-5 text-sm sm:text-base max-w-2xl mx-auto">
         Have an idea, project, or opportunity? I’m always open to meaningful
-        collaborations.
+        collaborations...! 🤗
       </p>
     </motion.div>
   );
