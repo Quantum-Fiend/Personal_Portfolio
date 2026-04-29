d03bce327ba3a7f4cc53e0285e0de02119ab4af4
Troff_Junkie <tusharbisht706@gmail.com>
2026-04-29 16:31:10 +0530

    Prject 3 Set and basic structure chnaged and cleanup

--- src/Components/features/home/project/projects.data.ts
+++ src/Components/features/home/project/projects.data.ts
@@ -8,26 +8,27 @@ export type Project = {
 
 export const projects: Project[] = [
   {
-    title: "Sensor_Net",
+    title: "Recruit_Flow",
     description:
-      "Sensor_Net is a full-stack IoT platform designed to bridge the gap between hardware sensors and actionable intelligence.",
-    image: "/project1.png",
-    githubUrl: "https://github.com/Quantum-Fiend/Sensor_Net",
+      "Project Status: 🚧 In Progress 🚧 This project is currently undergoing active development and production hardening.",
+    image:
+      "https://gratisography.com/wp-content/uploads/2024/11/gratisography-augmented-reality-800x525.jpg",
+
+    githubUrl: "https://github.com/Quantum-Fiend/Recruit_Flow",
   },
   {
     title: "Aurexis_Mobile",
     description:
-      "production-grade personal finance application showcasing an Elite Mobile Engineering Stack. It features native iOS (SwiftUI), native Android (Jetpack Compose),",
+      "production-grade personal finance application showcasing an Elite Mobile Engineering Stack. It features iOS (SwiftUI), Android.",
     image: "/project2.png",
 
     githubUrl: "https://github.com/Quantum-Fiend/aurexis-mobile",
   },
   {
-    title: "Task Manager App",
-    description: "Productivity app with drag & drop task management system.",
-    image:
-      "https://gratisography.com/wp-content/uploads/2024/11/gratisography-augmented-reality-800x525.jpg",
-
-    githubUrl: "#",
+    title: "Sensor_Net",
+    description:
+      "Sensor_Net is a full-stack IoT platform designed to bridge the gap between hardware sensors and actionable intelligence.",
+    image: "/project1.png",
+    githubUrl: "https://github.com/Quantum-Fiend/Sensor_Net",
   },
 ];
